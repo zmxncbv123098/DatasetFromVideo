e05450db4a9a782a9c8cdc093b4f6fdf6476c443
zmxncbv123098 <miketwin1@mail.ru>
2021-12-09 16:56:18 +0300

    add argparse \ Q - to abort

--- screenshot_from_video.py
+++ screenshot_from_video.py
@@ -1,5 +1,5 @@
 from functions import *
-
+import argparse
 import cv2
 """
 
@@ -11,110 +11,142 @@ B/N - Перемотка на +/- 600 кадров
 S - Сохранить фрэйм
 D - Удалить последний сохранённый фрейм
 
+Q - Закончить просмотр
 """
 
+
+def get_args():
+    parser = argparse.ArgumentParser("Yolo format annotations to COCO dataset format")
+    parser.add_argument(
+        "-v",
+        "--videos",
+        type=str,
+        help="Absolute path for the root dir for images.",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--ext",
+        type=str,
+        default="jpg",
+        help="Images extension",
+    )
+
+    args = parser.parse_args()
+    return args
+
 # Папка с исходными видео с соответствующим расширением
-videos_dir = "/home/mikhail/PycharmProjects/DatasetFromVideo/example"
-file_extension = ".mov"
+# videos_dir = "/home/mikhail/PycharmProjects/DatasetFromVideo/example"
+# file_extension = ".mov"
 
-# Пусть сохранения фреймов
-save_path = os.path.join(videos_dir, "dataset")
 
-videos_list = sorted(get_filelist(os.path.join(videos_dir, "raw"), ext=file_extension))
-last_saved = None
-waitKey_mode = 0
+def main(videos_dir, file_extension):
+    save_path = os.path.join(videos_dir, "dataset")
 
-os.makedirs(save_path, exist_ok=True)
+    videos_list = sorted(get_filelist(os.path.join(videos_dir, "raw"), ext=file_extension))
+    last_saved = None
+    waitKey_mode = 0
 
-# Счётчик для сохранённых фреймов
-counter = 0
-already_saved_imgs = get_filelist(save_path, ".jpg")
-for img_name in already_saved_imgs:
-    counter = max(counter, int(img_name.split("_")[-1].replace(".jpg", ""))) + 1
+    os.makedirs(save_path, exist_ok=True)
 
-video_id = 0
-for n, _ in enumerate(videos_list):
-    video = videos_list[video_id]
+    # Счётчик для сохранённых фреймов
+    counter = 0
+    already_saved_imgs = get_filelist(save_path, ".jpg")
+    for img_name in already_saved_imgs:
+        counter = max(counter, int(img_name.split("_")[-1].replace(".jpg", ""))) + 1
 
-    reader = Reader(cv2.VideoCapture(video))
+    video_id = 0
+    for n, _ in enumerate(videos_list):
+        video = videos_list[video_id]
 
-    frames = int(reader.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(video, "frames total:", frames)
+        reader = Reader(cv2.VideoCapture(video))
 
-    while True:
+        frames = int(reader.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print(video, "frames total:", frames)
 
-        frame = reader.read()
+        while True:
 
-        if frame is None:
-            break
+            frame = reader.read()
 
-        frame_id = int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES))
+            if frame is None:
+                break
 
-        save_img = frame.copy()
+            frame_id = int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES))
 
-        cv2.putText(frame, "frame {}/{}".format(frame_id, frames),
-                    (5, frame.shape[0] - 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 5)
+            save_img = frame.copy()
 
-        cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-        cv2.imshow("image", frame)
+            cv2.putText(frame, "frame {}/{}".format(frame_id, frames),
+                        (5, frame.shape[0] - 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 5)
 
-        key = cv2.waitKey(waitKey_mode) & 0xff
-        if key == ord('p'):
-            if waitKey_mode == 0:
-                waitKey_mode = 1
-                print("- play")
-            elif waitKey_mode == 1:
-                waitKey_mode = 0
-                print("- pause")
+            cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+            cv2.imshow("image", frame)
 
-        # Перемотка назад на -1 кадр
-        if key == ord('c'):
-            reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) - 3)
+            key = cv2.waitKey(waitKey_mode) & 0xff
+            if key == ord('p'):
+                if waitKey_mode == 0:
+                    waitKey_mode = 1
+                    print("- play")
+                elif waitKey_mode == 1:
+                    waitKey_mode = 0
+                    print("- pause")
 
-        # Перемотка вперед на +1 кадр
-        if key == ord('v'):
-            reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) + 3)
+            # Перемотка назад на -1 кадр
+            if key == ord('c'):
+                reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) - 3)
 
-        # Перемотка назад на -n кадров
-        if key == ord('z'):
-            reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) - 60)
+            # Перемотка вперед на +1 кадр
+            if key == ord('v'):
+                reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) + 3)
 
-        # Перемотка вперед на +n кадров
-        if key == ord('x'):
-            reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) + 60)
+            # Перемотка назад на -n кадров
+            if key == ord('z'):
+                reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) - 60)
 
-        # Перемотка назад на -n кадров
-        if key == ord('b'):
-            reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) - 600)
+            # Перемотка вперед на +n кадров
+            if key == ord('x'):
+                reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) + 60)
 
-        # Перемотка вперед на +n кадров
-        if key == ord('n'):
-            reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) + 600)
+            # Перемотка назад на -n кадров
+            if key == ord('b'):
+                reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) - 600)
 
-        # Предыдущее видео
-        if key == ord(','):
-            video_id = video_id - 2
-            break
+            # Перемотка вперед на +n кадров
+            if key == ord('n'):
+                reader.cap.set(propId=cv2.CAP_PROP_POS_FRAMES, value=int(reader.cap.get(cv2.CAP_PROP_POS_FRAMES)) + 600)
 
-        # Следующее видео
-        if key == ord('.'):
-            break
+            # Предыдущее видео
+            if key == ord(','):
+                video_id = video_id - 2
+                break
 
-        # Сохранить фрейм
-        if key == ord('s'):
-            save_name = "{}_{}.jpg".format(video.split("/")[-1].replace(file_extension, ""), counter)
-            last_saved = os.path.join(save_path, save_name)
-            print("- img saved to {}".format(os.path.join(save_path, save_name)))
-            cv2.imwrite(os.path.join(save_path, save_name), save_img)
-            counter += 1
+            # Следующее видео
+            if key == ord('.'):
+                break
 
-        # Удалить последний сохраненный фрейм
-        if key == ord('d'):
-            try:
-                os.remove(last_saved)
-                print("- {} deleted".format(last_saved))
-            except:
-                print("- Can`t delete {}".format(last_saved))
+            # Сохранить фрейм
+            if key == ord('s'):
+                save_name = "{}_{}.jpg".format(video.split("/")[-1].replace(file_extension, ""), counter)
+                last_saved = os.path.join(save_path, save_name)
+                print("- img saved to {}".format(os.path.join(save_path, save_name)))
+                cv2.imwrite(os.path.join(save_path, save_name), save_img)
+                counter += 1
 
-    video_id += 1
-    print()
+            # Удалить последний сохраненный фрейм
+            if key == ord('d'):
+                try:
+                    os.remove(last_saved)
+                    print("- {} deleted".format(last_saved))
+                except:
+                    print("- Can`t delete {}".format(last_saved))
+
+            if key == ord('q'):
+                print("Aborting")
+                exit(1)
+
+        video_id += 1
+        print()
+
+
+if __name__ == "__main__":
+    options = get_args()
+    main(options.videos, options.ext)
